cf393688b93c7a15d94c647ec6202bfeaf1b1a15
Kaishiro1996 <marcelo_ema@hotmail.com>
2025-06-24 14:44:30 -0600

    Improve logging in GET handler to include result details

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -73,7 +73,7 @@ export async function GET() {
             await seedUsers();
             await seedTeams();
           });
-          console.log(result); // O hacer algo útil con él
+          console.log("result",result); // O hacer algo útil con él
         return Response.json({ message: 'Database seeded successfully' });
     } catch (error) {
       return Response.json({ error }, { status: 500 });
