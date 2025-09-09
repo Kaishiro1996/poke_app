c609c669ca5bef49e81817b9692f97127293113f
Kaishiro1996 <marcelo_ema@hotmail.com>
2025-09-09 12:37:00 -0600

    feat: enhance Post for adding pokemon

--- src/app/api/pokemon/addPokemon/route.ts
+++ src/app/api/pokemon/addPokemon/route.ts
@@ -17,27 +17,31 @@ export async function POST(req: NextRequest) {
             teratype,
             shiny,
             ability,
-            iv_hp,
-            iv_atq,
-            iv_def,
-            iv_spa,
-            iv_spd,
-            iv_spe,
-            ev_hp,
-            ev_atq,
-            ev_def,
-            ev_spa,
-            ev_spd,
-            ev_spe,
+            iv_Hp,
+            iv_Atk,
+            iv_Def,
+            iv_SpA,
+            iv_Spd,
+            iv_Spe,
+            ev_Hp,
+            ev_Atk,
+            ev_Def,
+            ev_SpA,
+            ev_Spd,
+            ev_Spe,
+            nature,
             id_trainer,
             id_team
         } = body;
-        if (  !name || !gender || !teratype ||  !nickname || !pokedex_number || !move1 || !move2 || !move3 || !move4 || shiny === undefined || !ability || !id_trainer || !id_team || !iv_hp || !iv_atq || !iv_def || !iv_spa || !iv_spd || !iv_spe || !ev_hp || !ev_atq || !ev_def || !ev_spa || !ev_spd || !ev_spe) {
+        if (  !name ||  !teratype || !move1 || shiny === undefined || !ability || !id_trainer || !id_team  ) {
             return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
         }
+        const realGender = body.gender === true ? 'male' : 'female';
+     
+
         const [result] = await sql`
-        INSERT INTO pokemon (name, nickname, pokedex_number, move1, move2, move3, move4, shiny, ability, id_trainer, id_team, gender, teratype, iv_hp, iv_atq, iv_def, iv_spa, iv_spd, iv_spe, ev_hp, ev_atq, ev_def, ev_spa, ev_spd, ev_spe)
-        VALUES ( ${name}, ${nickname}, ${pokedex_number}, ${move1}, ${move2}, ${move3}, ${move4}, ${shiny}, ${ability}, ${id_trainer}, ${id_team} , ${gender}, ${teratype}, ${iv_hp}, ${iv_atq}, ${iv_def}, ${iv_spa}, ${iv_spd}, ${iv_spe}, ${ev_hp}, ${ev_atq}, ${ev_def}, ${ev_spa}, ${ev_spd}, ${ev_spe})
+        INSERT INTO pokemon (name, nickname, pokedex_number, move1, move2, move3, move4, shiny, ability, id_trainer, id_team, gender, teratype, iv_hp, iv_atq, iv_def, iv_spa, iv_spd, iv_spe, ev_hp, ev_atq, ev_def, ev_spa, ev_spd, ev_spe, nature)
+        VALUES ( ${name}, ${nickname}, ${pokedex_number}, ${move1}, ${move2}, ${move3}, ${move4}, ${shiny}, ${ability}, ${id_trainer}, ${id_team} , ${realGender}, ${teratype}, ${iv_Hp}, ${iv_Atk}, ${iv_Def}, ${iv_SpA}, ${iv_Spd}, ${iv_Spe}, ${ev_Hp}, ${ev_Atk}, ${ev_Def}, ${ev_SpA}, ${ev_Spd}, ${ev_Spe}, ${nature})
         RETURNING id, nickname, name;
         `;
         return NextResponse.json({ pokemonId: result.id }, { status: 201 });
