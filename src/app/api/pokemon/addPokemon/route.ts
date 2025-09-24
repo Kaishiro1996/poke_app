8efb89ba5815a2c50be7e2d766cbd9d4561bdcc0
Kaishiro1996 <marcelo_ema@hotmail.com>
2025-09-24 17:37:53 -0600

    feat: Add item to addPokemon

--- src/app/api/pokemon/addPokemon/route.ts
+++ src/app/api/pokemon/addPokemon/route.ts
@@ -30,6 +30,7 @@ export async function POST(req: NextRequest) {
             ev_Spd,
             ev_Spe,
             nature,
+            item,
             id_trainer,
             id_team
         } = body;
@@ -37,11 +38,10 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
         }
         const realGender = body.gender === true ? 'male' : 'female';
-     
 
         const [result] = await sql`
-        INSERT INTO pokemon (name, nickname, pokedex_number, move1, move2, move3, move4, shiny, ability, id_trainer, id_team, gender, teratype, iv_hp, iv_atq, iv_def, iv_spa, iv_spd, iv_spe, ev_hp, ev_atq, ev_def, ev_spa, ev_spd, ev_spe, nature)
-        VALUES ( ${name}, ${nickname}, ${pokedex_number}, ${move1}, ${move2}, ${move3}, ${move4}, ${shiny}, ${ability}, ${id_trainer}, ${id_team} , ${realGender}, ${teratype}, ${iv_Hp}, ${iv_Atk}, ${iv_Def}, ${iv_SpA}, ${iv_Spd}, ${iv_Spe}, ${ev_Hp}, ${ev_Atk}, ${ev_Def}, ${ev_SpA}, ${ev_Spd}, ${ev_Spe}, ${nature})
+        INSERT INTO pokemon (name, nickname, pokedex_number, move1, move2, move3, move4, shiny, ability, id_trainer, id_team, gender, teratype, iv_hp, iv_atq, iv_def, iv_spa, iv_spd, iv_spe, ev_hp, ev_atq, ev_def, ev_spa, ev_spd, ev_spe, nature, item)
+        VALUES ( ${name}, ${nickname}, ${pokedex_number}, ${move1}, ${move2}, ${move3}, ${move4}, ${shiny}, ${ability}, ${id_trainer}, ${id_team} , ${realGender}, ${teratype}, ${iv_Hp}, ${iv_Atk}, ${iv_Def}, ${iv_SpA}, ${iv_Spd}, ${iv_Spe}, ${ev_Hp}, ${ev_Atk}, ${ev_Def}, ${ev_SpA}, ${ev_Spd}, ${ev_Spe}, ${nature} , ${item})
         RETURNING id, nickname, name;
         `;
         return NextResponse.json({ pokemonId: result.id }, { status: 201 });
